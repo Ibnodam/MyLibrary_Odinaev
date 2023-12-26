10adda88b7228ba21f440d11db06ae946c66aef8
Ibnodam <valijonodinaev7@gmail.com>
2023-12-26 03:18:13 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,14 @@
+﻿double[,] mas = new double[5, 5]; //// Т.к. ни одно значение не вводится с клавиатуры, то не буду страховаться 
+Random rand = new Random();
+Console.WriteLine("Исходная матрица: ");
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas[i, j] = (rand.Next(0, 256)) + (rand.NextDouble()); // Генерируем случайное число с плавающей точкой
+        Console.Write($"|  {(mas[i, j]):F2}  |");
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine($"Среднее арифметическое число побочной диагонали равно {(ClassLibrary1.Library.Avg(mas)):F2}");
